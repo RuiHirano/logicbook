0a5866e48c755c1759fa4aaaf6f8ce2ad0cd1eb9
Rui Hirano <r.hrn.0930@gmail.com>
2022-03-20 08:24:25 +0900

    update v0.1.0

--- setup.py
+++ setup.py
@@ -9,15 +9,16 @@ install_requires = [x.strip() for x in all_reqs]
 
 setup(
     name="logicbook",
-    version="0.0.8",
+    version="0.1.0",
     packages=["logicbook"],
     include_package_data=True,
     entry_points={
             "console_scripts": ['logicbook = logicbook.cli.root:main']
         },
     install_requires = install_requires,
-    description='Logicbook CLI',
+    description='Logicbook is an open source tool for building python logics in isolation. It streamlines logic development, testing, and documentation.',
     long_description=readme,
     author='Rui Hirano',
     license='MIT',
+    url='https://ruihirano.github.io/logicbook/',
 )
