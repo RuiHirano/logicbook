9587557a7ed051c694f219e34ca94f3c9a9b86ed
Rui Hirano <r.hrn.0930@gmail.com>
2022-03-21 17:39:26 +0900

    fix hotfix and ready for 0.1.3

--- logicbook/template/logics/logic1/test_logic1.py
+++ logicbook/template/logics/logic1/test_logic1.py
@@ -5,13 +5,13 @@ class TestSum(unittest.TestCase):
 
     def test_sum(self):
         sum_logic = SumLogic(1, 2)
-        actual = sum_logic.sum()
-        self.assertEqual(3, actual)
+        actual = sum_logic.sum(2)
+        self.assertEqual(5, actual)
 
     def test_sum2(self):
         sum_logic = SumLogic(4, 2)
-        actual = sum_logic.sum()
-        self.assertEqual(6, actual)
+        actual = sum_logic.sum(4)
+        self.assertEqual(10, actual)
 
 
 if __name__ == '__main__':
